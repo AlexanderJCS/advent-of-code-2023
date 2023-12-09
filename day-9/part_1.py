5b8b374c7a07e282afceb3733f65e61d2524ba82
Alex <alexandercastronovo@gmail.com>
2023-12-09 11:58:16 -0500

    shorten more

--- day-9/part_1.py
+++ day-9/part_1.py
@@ -1,24 +1,12 @@
 
 
-def get_diff(ints: list[int]) -> list[int]:
-    return [ints[i] - ints[i - 1] for i in range(1, len(ints))]
-
-
 def get_diffs(oasis_report: list[int]) -> list[list[int]]:
     diffs = [oasis_report]
 
     while any(diff != 0 for diff in diffs[-1]):
-        diffs.append(get_diff(diffs[-1]))
+        diffs.append([diffs[-1][i] - diffs[-1][i - 1] for i in range(1, len(diffs[-1]))])
 
     return diffs
-
-
-def parse_line(line: str) -> list[int]:
-    return list(map(int, line.split()))
-
-
-def predict_next_value(diffs: list[list[int]]) -> int:
-    return sum(diff[-1] for diff in diffs)
 
 
 def main():
