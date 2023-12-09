e1771c141174f346f7ab420aa3c10232731b52ff
Alex <alexandercastronovo@gmail.com>
2023-12-09 12:53:19 -0500

    shorten to a 2 liner

--- day-9/part_1.py
+++ day-9/part_1.py
@@ -1,10 +1,8 @@
 
 
 def get_diffs(oasis_report: list[int]) -> list[list[int]]:
-    diffs = [oasis_report]
-
-    while any(diff != 0 for diff in diffs[-1]):
-        diffs.append([diffs[-1][i] - diffs[-1][i - 1] for i in range(1, len(diffs[-1]))])
+    last_report = oasis_report
+    diffs = [oasis_report] + [(last_report := [last_report[i] - last_report[i - 1] for i in range(1, len(last_report))]) for _ in range(len(oasis_report) - 1)]
 
     return diffs
 
