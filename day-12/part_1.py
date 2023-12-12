6190eded21371c1606e5639149c7abff94c456f5
Alex <alexandercastronovo@gmail.com>
2023-12-12 13:05:28 -0500

    Add day 12 part 1

---
+++ day-12/part_1.py
@@ -0,0 +1,117 @@
+
+
+def is_valid_line(line: str | list[str], hashtags: list[int]) -> bool:
+    if line == ".###...##..#":
+        pass
+
+    max_hashtags = max(hashtags)
+
+    hashtags_in_row = []
+
+    hashtag_count = 0
+    for element in line:
+        if element == "#":
+            hashtag_count += 1
+
+        elif hashtag_count != 0:
+            # Exit early if you can
+            if hashtag_count > max_hashtags:
+                return False
+
+            hashtags_in_row.append(hashtag_count)
+            hashtag_count = 0
+
+    if hashtag_count != 0:
+        hashtags_in_row.append(hashtag_count)
+
+    return hashtags_in_row == hashtags
+
+
+def gen_combinations(s: str | list[str], current="", index=0):
+    if index == len(s):
+        yield current
+
+    elif s[index] == "?":
+        yield from gen_combinations(s, current + ".", index + 1)
+        yield from gen_combinations(s, current + "#", index + 1)
+
+    else:
+        yield from gen_combinations(s, current + s[index], index + 1)
+
+
+def bruteforce(line: list[str], hashtags: list[int]) -> int:
+    line = [element for element in line]
+
+    combinations = gen_combinations(line)
+
+    valid_combinations = 0
+    for combination in combinations:
+        if is_valid_line(combination, hashtags):
+            valid_combinations += 1
+
+    return valid_combinations
+
+
+def has_too_many_in_row(line: list[str], max_hashtags: int) -> bool:
+    """
+    :return: True if the number of hashtags in a row is greater than the maximum
+    """
+    if line[-2] == ".":
+        pass
+
+    num_in_row = 0
+    for element in line:
+        if element == "#":
+            num_in_row += 1
+
+        elif element != "#":
+            num_in_row = 0
+
+        if num_in_row > max_hashtags:
+            return True
+
+    return False
+
+
+def find_num_arrangements(line: list[str], hashtags: list[int]) -> int:
+    line = [element for element in line]
+
+    # First find all spaces that must be whitespace to reduce load when bruteforcing
+    for i, element in enumerate(line):
+        if element != "?":
+            continue
+
+        line[i] = "#"
+
+        if has_too_many_in_row(line, max(hashtags)):
+            line[i] = "."
+
+        else:
+            line[i] = "?"
+
+    # Bruteforce
+    return bruteforce(line, hashtags)
+
+
+def parse_line(line: str) -> tuple[list[str], list[int]]:
+    splitted = line.split()
+
+    return list(splitted[0]), list(map(int, splitted[1].split(",")))
+
+
+def main():
+    with open("input.txt") as f:
+        lines = f.read().splitlines()
+
+    sum_arrangements = 0
+
+    for line in lines:
+        springs, hashtags = parse_line(line)
+
+        sum_arrangements += find_num_arrangements(springs, hashtags)
+
+    print(sum_arrangements)
+
+
+if __name__ == "__main__":
+    main()
